3e9ac78afe2a3fdc9c85d89c013943289ee7d44b
Ivan Manzhosov <manzhiva@fit.cvut.cz>
2024-03-29 00:00:38 +0100

    feat: create user and media entities #8

---
+++ api/src/main/kotlin/cz/cvut/fit/sp1/api/data/model/UserAccount.kt
@@ -0,0 +1,25 @@
+package cz.cvut.fit.sp1.api.data.model
+
+import cz.cvut.fit.sp1.api.data.model.base.StandardAuditModel
+import cz.cvut.fit.sp1.api.data.model.media.Media
+import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
+import jakarta.persistence.ManyToMany
+import jakarta.persistence.OneToMany
+import org.hibernate.annotations.ColumnDefault
+
+@Entity
+class UserAccount(
+    var name: String = "",
+    var email: String = "",
+    var password: String = "",
+) : StandardAuditModel() {
+    @ColumnDefault("2")
+    var role: AccountRole = AccountRole.USER
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    var likedMedia: MutableList<Media> = mutableListOf()
+
+    @OneToMany(mappedBy = "createdBy", fetch = FetchType.LAZY)
+    var createdMedia: MutableList<Media> = mutableListOf()
+}
