162de456365fc7322a729b5f6382cfb21797a44b
Matyas Vascak <matyasvascak@gmail.com>
2024-04-11 22:43:32 +0200

    UserRepository basic Impl

---
+++ api/build/classes/kotlin/main/cz/cvut/fit/sp1/api/data/repository/UserRepository.kt
@@ -0,0 +1,8 @@
+// IntelliJ API Decompiler stub source generated from a class file
+// Implementation of methods is not available
+
+package cz.cvut.fit.sp1.api.data.repository
+
+public interface UserRepository : org.springframework.data.jpa.repository.JpaRepository<cz.cvut.fit.sp1.api.data.model.media.User, kotlin.Long> {
+}
+
