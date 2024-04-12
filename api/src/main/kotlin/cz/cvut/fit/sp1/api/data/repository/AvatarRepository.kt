4f97967d72a1d975b3a9d002bcafafd11044650a
Matyas Vascak <matyasvascak@gmail.com>
2024-04-12 14:46:42 +0200

    AvatarRepository  implementation

--- api/src/main/kotlin/cz/cvut/fit/sp1/api/data/repository/AvatarRepository.kt
+++ api/src/main/kotlin/cz/cvut/fit/sp1/api/data/repository/AvatarRepository.kt
@@ -1,6 +1,6 @@
 package cz.cvut.fit.sp1.api.data.repository
 
-import cz.cvut.fit.sp1.api.data.model.media.Video
+import cz.cvut.fit.sp1.api.data.model.media.Mask
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface UserRepository : JpaRepository<Video, Long>
+interface AvatarRepository : JpaRepository<Mask, Long>
