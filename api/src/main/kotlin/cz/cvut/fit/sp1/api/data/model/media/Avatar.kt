cfdf297d69d6bf188fe15146abe6bfc171fb19cc
Matyas Vascak <matyasvascak@gmail.com>
2024-04-12 16:16:26 +0200

    Avatar getting rid of media

--- api/src/main/kotlin/cz/cvut/fit/sp1/api/data/model/media/Avatar.kt
+++ api/src/main/kotlin/cz/cvut/fit/sp1/api/data/model/media/Avatar.kt
@@ -1,11 +1,17 @@
 package cz.cvut.fit.sp1.api.data.model.media
 
+import cz.cvut.fit.sp1.api.data.model.UserAccount
+import cz.cvut.fit.sp1.api.data.model.base.StandardAuditModel
 import jakarta.persistence.Entity
+import jakarta.persistence.OneToOne
 
 @Entity
 class Avatar(
-    name: String,
-    path: String,
-    format: String,
-    aspecRatio: Double,
-) : Media(name, path, format)
+    var name: String,
+    var path: String,
+    var height: Int,
+    var width: Int
+) : StandardAuditModel() {
+    @OneToOne
+    var userAccount: UserAccount? = null
+}
