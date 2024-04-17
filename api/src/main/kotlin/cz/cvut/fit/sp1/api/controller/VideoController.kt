c769422d2e2443d05aa6b4a5dc185976327658d9
Ivan Manzhosov <manzhiva@fit.cvut.cz>
2024-04-17 22:44:19 +0200

    feat: create video with tag

--- api/src/main/kotlin/cz/cvut/fit/sp1/api/controller/VideoController.kt
+++ api/src/main/kotlin/cz/cvut/fit/sp1/api/controller/VideoController.kt
@@ -2,12 +2,12 @@ package cz.cvut.fit.sp1.api.controller
 
 import cz.cvut.fit.sp1.api.component.mapper.VideoMapper
 import cz.cvut.fit.sp1.api.data.dto.VideoDto
+import cz.cvut.fit.sp1.api.data.dto.VideoDtoRequest
 import cz.cvut.fit.sp1.api.data.dto.search.SearchMediaParamsDto
 import cz.cvut.fit.sp1.api.data.dto.search.SearchVideoDto
 import cz.cvut.fit.sp1.api.data.model.media.Video
 import cz.cvut.fit.sp1.api.data.service.interfaces.VideoService
 import jakarta.annotation.security.RolesAllowed
-import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
 
@@ -32,12 +32,13 @@ class VideoController(
         return videoService.findAll(paramsDto)
     }
 
-    @PostMapping(consumes = [MediaType.MULTIPART_FORM_DATA_VALUE])
+    @PostMapping
     @RolesAllowed("USER", "ADMIN")
     fun upload(
         @RequestParam("video") videoFile: MultipartFile,
+        @ModelAttribute videoDto: VideoDtoRequest,
     ): VideoDto? {
-        val video = videoService.create(videoFile)
+        val video = videoService.create(videoFile, videoDto)
         return videoMapper.toDto(video)
     }
 }
