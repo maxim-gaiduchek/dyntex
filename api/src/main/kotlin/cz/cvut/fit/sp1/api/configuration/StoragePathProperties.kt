d3a48dae6d8a09fad8c3bf545d014d09f5384944
maxim-gaiduchek <maxim.gayduchek@gmail.com>
2024-04-24 13:36:59 +0200

    feature(media): Add user DTO in media DTO
    Implements: #34

--- api/src/main/kotlin/cz/cvut/fit/sp1/api/configuration/StoragePathProperties.kt
+++ api/src/main/kotlin/cz/cvut/fit/sp1/api/configuration/StoragePathProperties.kt
@@ -1,8 +1,9 @@
 package cz.cvut.fit.sp1.api.configuration
 
 import org.springframework.boot.context.properties.ConfigurationProperties
+import kotlin.io.path.Path
 
 @ConfigurationProperties(prefix = "storage")
 class StoragePathProperties {
-    var mediaPath: String = "/Users/maksymgaiduchek/Library/CloudStorage/OneDrive-Личная/Universities/CTU/SP1/storage"
+    var mediaPath: String = Path(System.getProperty("user.home"), "sp1", "storage").toString()
 }
