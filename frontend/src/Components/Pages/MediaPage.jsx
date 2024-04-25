7262ec556bb6d77281b47b5d0a923d0238083cfb
Ivan Stojka <stojkiva@fit.cvut.cz>
2024-04-25 11:19:54 +0200

    texutre video preview #9

--- frontend/src/Components/Pages/MediaPage.jsx
+++ frontend/src/Components/Pages/MediaPage.jsx
@@ -32,15 +32,25 @@ export default function MediaPage(){
             texture != null ?
             <>
                 <Group justify="center" style={{backgroundColor: "black", padding: "0"}} grow>
-                    <img
+                    {/* <img
                         style={{
                             width: "100%",
                             maxWidth: 500
                         }}
-                        alt="texture" src={"http://localhost:8080/api/videos/previews/" + texture.previewPath}/>
+                        alt="texture" src={"http://localhost:8080/api/media/previews/" + texture.previewPath}/> */}
+                    <video controls className={classes.video}
+                        loop
+                        autoPlay 
+                        style={{
+                            width: "100%",
+                            maxWidth: 600
+                        }}>
+                        <source src={"http://localhost:8080/api/media/stream/" + texture.path} type="video/mp4" />
+                        Your browser does not support the video tag.
+                    </video>
                 </Group>
                 <Group mt="xs" justify="left">
-                    <Link to={"http://localhost:8080/api/videos/stream/"+texture.path} target="_blank">
+                    <Link to={"http://localhost:8080/api/videos/download/"+texture.path} target="_blank">
                         <Button radius="md" rightSection={<IconDownload size={14} />} style={{width: 300}}>
                             Download
                         </Button>
