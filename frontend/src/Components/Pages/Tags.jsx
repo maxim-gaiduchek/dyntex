031364100af23a4c767411fe7067488bf6de16ca
Ivan Stojka <stojkiva@fit.cvut.cz>
2024-04-26 16:22:05 +0200

    modal for adding tags #9

--- frontend/src/Components/Pages/Tags.jsx
+++ frontend/src/Components/Pages/Tags.jsx
@@ -1,12 +1,22 @@
 import { useState, useEffect } from "react"
 import axios from "axios"
-import { Card, Grid } from "@mantine/core"
+import { Card, Grid, TextInput } from "@mantine/core"
 import { UnstyledButton, Text } from "@mantine/core"
 import classes from "./Tags.module.css"
 import { SimpleGrid, Button } from "@mantine/core"
+import { useDisclosure } from "@mantine/hooks"
+import { Modal } from "@mantine/core"
+import EmojiPicker from 'emoji-picker-react';
+import { useComputedColorScheme } from "@mantine/core"
+import { HoverCard } from "@mantine/core"
+import { IconMoodEmpty } from "@tabler/icons-react"
 
 export default function Tags() {
     const [tags, setTags] = useState([])
+    const [opened, { open, close }] = useDisclosure(false);
+    const [emoji, setEmoji] = useState("")
+    const [name, setName] = useState("")
+    const computedColorScheme = useComputedColorScheme('light', { getInitialValueInEffect: true });
 
     const fetchData = async () => {
         try{
@@ -22,8 +32,31 @@ export default function Tags() {
 
     return (
         <>
+            <Modal opened={opened} onClose={close} title="Adding Tag">
+                <TextInput value={name} onChange={(e) => setName(e.target.value)} placeholder="Name"/>
+                <br/>
+                <HoverCard width={280} openDelay={0}>
+                    <HoverCard.Target>
+                        <TextInput value={emoji} placeholder="Emoji (hover for keyboard)"/>
+                    </HoverCard.Target>
+                    <HoverCard.Dropdown className={classes.mantineHoverCardDropdown} style={{background: "none !important"}}>
+                        <EmojiPicker
+                            defaultCaption=""
+                            theme={computedColorScheme}
+                            height={400}
+                            onEmojiClick={
+                                (e) => {
+                                    setEmoji(e.emoji)
+                                }
+                            }
+                        />
+                    </HoverCard.Dropdown>
+                </HoverCard>
+                <br/>
+                <Button onClick={() => {alert("еще не сделал сори)")}} disabled={emoji === "" || name === ""}>Add Tag</Button>
+            </Modal>
             <h2>Tags</h2>
-            <Button>Add Tag</Button>
+            <Button onClick={open}>Add Tag</Button>
             <Card withBorder radius="md" className={classes.card}>
                 <Grid>
                     {
