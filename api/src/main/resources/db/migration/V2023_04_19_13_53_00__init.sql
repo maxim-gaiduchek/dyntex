6c1c0d90be7a7e7781c29ee1d7bfe1e256b7b8e5
maxim-gaiduchek <maxim.gayduchek@gmail.com>
2024-10-07 09:25:20 +0200

    feature(security): Fix migration script
    Implements: #45

--- api/src/main/resources/db/migration/V2023_04_19_13_53_00__init.sql
+++ api/src/main/resources/db/migration/V2023_04_19_13_53_00__init.sql
@@ -1,7 +1,8 @@
-insert into user_account
-values (1, now(), now(), true, ' ', 'kvoza@gmail.com', 'Kvoza Onkay',
-        '44136fa355b3678a1146ad16f7e8649e94fb4fc21fe77e8310c060f61caaff8a', 0, 'yLUSznFONVgwPBnmXyVgDpyBlnJFTykA',
-        now());
+insert into user_account (id, created_at, updated_at, auth_enable, auth_token, date_of_recovery, email, name, password,
+                          role, token)
+values (1, now(), now(), true, 'auth_token', now(),
+        'kvoza@gmail.com', 'Kvoza Onkay',
+        '44136fa355b3678a1146ad16f7e8649e94fb4fc21fe77e8310c060f61caaff8a', 0, 'token');
 
 insert into tag
 values (1, now(), now(), '🏕', 'Nature', 1);
